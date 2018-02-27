6e5035b0de4d6c2dfcb7962b4a08c016acb843fa
Manuel Garcia <manuel.drupal@gmail.com>
2018-02-27 13:07:57 +0000

    New ViewProfilesPermsAccessHandler overrides user access handler class

---
+++ src/ViewProfilesPermsAccessHandler.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\view_profiles_perms;
+
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\user\UserAccessControlHandler;
+
+/**
+ * Defines an access control handler for the user entity type.
+ */
+class ViewProfilesPermsAccessHandler extends UserAccessControlHandler {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function checkAccess(EntityInterface $entity, $operation, AccountInterface $account) {
+    // Parent class allowing access takes precedence.
+    /** @var \Drupal\user\UserInterface $entity*/
+    $access = parent::checkAccess($entity, $operation, $account);
+    if ($access->isAllowed()) {
+      return $access;
+    }
+    // Respect the conditions on view of the parent class.
+    // @see \Drupal\user\UserAccessControlHandler::checkAccess()
+    if ($operation == 'view' && $entity->isActive() && ($account->id() !== $entity->id())) {
+      foreach ($entity->getRoles(TRUE) as $role) {
+        return $access->allowedIfHasPermission($account, "access $role profiles");
+      }
+    }
+    return $access;
+  }
+
+}
